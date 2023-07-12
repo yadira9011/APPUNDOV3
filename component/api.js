28a75194e69e1f941b73dd146967609be49e65a0
Yadira Perez Castillo <yadira.perez@rodac.com.mx>
2023-07-12 16:21:37 -0600

    preubaaa

--- component/api.js
+++ component/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+
 export const loginUser = (email, password) => {
   const credential = {
     Usuario:email,
     Contraseña: password,
     Check: true
   };
+  console.log(BASE_URL);
   return axios.post(`${BASE_URL}/API/Login/SignIn`,credential);
 };
