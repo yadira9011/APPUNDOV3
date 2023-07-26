e2c46c6fa95b623a452a8800a3330d1d6247bd69
Windows 10 <yadira.perez@rodac.com.mx>
2023-07-25 23:25:31 -0600

    cambiops hoy nocheee home scren

--- component/HomeScreen.js
+++ component/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, SectionList, ActivityIndicator } from 'react-native';
+import { View, Text, SectionList, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { UserGrupos } from './api';
 
 const HomeScreen = ({ route }) => {
@@ -8,6 +8,7 @@ const HomeScreen = ({ route }) => {
   const [userData, setUserData] = useState([]);
 
   useEffect(() => {
+    
     const fetchData = async () => {
       try {
         const response = await UserGrupos(
@@ -16,19 +17,14 @@ const HomeScreen = ({ route }) => {
           userDataParameter.IdUsr
         );
 
-        // No es necesario usar trim() en response.data.GrupoEmpresas
+        // Verificar si la respuesta contiene GrupoEmpresas
+        if (response.data.GrupoEmpresas) {
+          // Crear una única sección con todos los elementos en userData
+          const data = [{ title: 'Grupo Empresa', data: response.data.GrupoEmpresas, expanded: false }];
 
-        // Verificar si response.data.GrupoEmpresas es un array de objetos válido
-        try {
-          const jsonData = JSON.parse(response.data.GrupoEmpresas);
-          if (Array.isArray(jsonData)) {
-            setUserData(jsonData);
-            setLoading(false);
-          } else {
-            console.error('La cadena JSON no es un array de objetos válido.');
-          }
-        } catch (error) {
-          console.error('La cadena JSON no es válida:', error);
+          setUserData(data);
+        } else {
+          console.error('La respuesta de la API no contiene GrupoEmpresas.');
         }
 
         setLoading(false);
@@ -37,6 +33,7 @@ const HomeScreen = ({ route }) => {
         setLoading(false);
       }
     };
+
     fetchData();
   }, []);
 
@@ -48,24 +45,76 @@ const HomeScreen = ({ route }) => {
     );
   }
 
+  // Función toggleSection movida fuera del componente HomeScreen
+  const toggleSection = (section) => {
+    const updatedData = userData.map((item) => {
+      if (item.title === section.title) {
+        return { ...item, expanded: !item.expanded };
+      }
+      return item;
+    });
+    setUserData(updatedData);
+  };
+
+  const ExpandableItem = ({ item, onPress, index}) => {
+    // console.log("entre a expanded....")
+    // console.log(index)
+    // console.log(item)
+    // console.log(item[0].expanded)
+    // console.log(item[0].data[index].Grupo)
+    if (item[0].expanded) {
+      return (
+        <TouchableOpacity onPress={onPress}>
+          <View style={styles.itemContainer}>
+            <Text style={styles.itemText}>{item[0].data[index].Grupo}</Text>
+          </View>
+        </TouchableOpacity>
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <SectionList
         sections={userData}
         keyExtractor={(item, index) => item.IdGrupoEmpresa.toString()}
-        renderItem={({ item }) => (
-          <View style={{ padding: 10 }}>
-            <Text>{item.Grupo}</Text>
-          </View>
+        renderItem={({ item, index }) => (
+          <ExpandableItem item={userData} onPress={() => toggleSection(item)} index={index} />
         )}
-        renderSectionHeader={({ section: { title } }) => (
-          <View style={{ backgroundColor: '#f0f0f0', padding: 10 }}>
-            <Text style={{ fontWeight: 'bold' }}>{title}</Text>
-          </View>
+        renderSectionHeader={({ section }) => (
+          <TouchableOpacity onPress={() => toggleSection(section)}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>{section.title}</Text>
+            </View>
+          </TouchableOpacity>
         )}
       />
     </View>
   );
+};
+
+const styles = {
+  // Estilos de la lista
+  itemContainer: {
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  itemText: {
+    fontSize: 16,
+  },
+  sectionHeader: {
+    backgroundColor: '#007bff', // Color para el encabezado de sección
+    padding: 10,
+  },
+  sectionHeaderText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff', // Color para el texto del encabezado de sección
+  },
 };
 
 export default HomeScreen;
