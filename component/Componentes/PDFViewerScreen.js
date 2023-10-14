e28deb79a0c8b4a37610100bbfcc6d24da0bd1a2
YADIRA JAZMIN PEREZ CASTILLO <yadira.perez@rodac.com.mx>
2023-10-14 14:43:30 -0600

    cambios en impresión de poliza cool

--- component/Componentes/PDFViewerScreen.js
+++ component/Componentes/PDFViewerScreen.js
@@ -2,59 +2,140 @@
 // import { View, StyleSheet } from 'react-native';
 //import Pdf from 'react-native-pdf';
 //import Pdf from 'react-native-pdf';
-
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Button, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as Sharing from 'expo-sharing';
+import * as FileSystem from 'expo-file-system';
+import LoadingComponent from './LoadingComponent';
+// import RNFS from 'react-native-fs';
+// import Pdf from 'react-native-pdf';
+// import PDFReader from '@bildau/rn-pdf-reader'
 
 const PDFViewerScreen = ({ route }) => {
 
     const { pdfUrl } = route.params;
+    const [loading, setLoading] = useState(false);
     console.log("urllll", pdfUrl)
 
+    // const sharePDF = async () => {
+    //     try {
+    //         const options = {
+    //             url: pdfUrl,
+    //         };
+    //         await Share.open(options);
+    //     } catch (error) {
+    //         console.error('Error al compartir el archivo PDF:', error);
+    //     }
+    // };
+
+    // const sharePDF = async () => {
+    //     try {
+    //         await Sharing.shareAsync(pdfUrl);
+    //     } catch (error) {
+    //         console.error('Error al compartir el enlace del PDF:', error);
+    //     }
+    // };
+
+    const sharePDF = () => {
+        Linking.openURL(pdfUrl)
+            .then((supported) => {
+                if (!supported) {
+                    console.log(`No se puede abrir el enlace: ${pdfUrl}`);
+                } else {
+                    console.log(`El enlace se ha abierto correctamente: ${pdfUrl}`);
+                }
+            })
+            .catch((error) => {
+                console.error(`Error al abrir el enlace: ${pdfUrl}`, error);
+            });
+    };
+
+    const downloadAndSharePDF = async () => {
+
+        try {
+            setLoading(true);
+            const pdfUri = `${FileSystem.documentDirectory}sample.pdf`;
+            const response = await FileSystem.downloadAsync(pdfUrl, pdfUri);
+            if (response.status === 200) {
+                setLoading(false);
+                console.log(`Archivo PDF descargado y guardado en: ${pdfUri}`);
+
+                Sharing.shareAsync(pdfUri)
+                    .then((result) => {
+                        if (result.action === Sharing.sharedAction) {
+                            console.log('Archivo compartido exitosamente');
+                            FileSystem.deleteAsync(pdfUri, { idempotent: true })
+                                .then(() => {
+                                    console.log('Archivo eliminado exitosamente');
+                                })
+                                .catch((deleteError) => {
+                                    console.error('Error al eliminar el archivo:', deleteError);
+                                });
+                        } else {
+                            console.log('Compartir cancelado');
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error al compartir el archivo PDF:', error);
+                    });
+            } else {
+                console.error('Error al descargar el archivo PDF');
+            }
+        } catch (error) {
+            console.error('Error al descargar y compartir el archivo PDF:', error);
+        }
+    };
+
     return (
+
         <View style={styles.container}>
 
             <WebView
-                source={{ pdfUrl }}
-                scalesPageToFit={true}
-                originWhitelist={['*']}
-                allowsFullscreenVideo={true}
+                style={{ height: 200, width: 350 }}
+                nestedScrollEnabled={true}
                 javaScriptEnabled={true}
-                injectedJavaScript={'(function() { })()'}
-            />
+                //  source={{ uri: 'https://drive.google.com/viewerng/viewer?embedded=true&url=http://www.africau.edu/images/default/sample.pdf' }}
+                source={{ uri: 'https://drive.google.com/viewerng/viewer?embedded=true&url=' + pdfUrl + '' }} />
 
-            {/* <PDFReader
+            <Button title="Compartir PDF" onPress={downloadAndSharePDF} />
+            {loading && (
+                <LoadingComponent />
+            )}
+
+
+            {/* 
+            <PDFReader
                 source={{
                     uri: 'http://gahp.net/wp-content/uploads/2017/09/sample.pdf',
                 }}
             /> */}
-            {/* <Pdf
-                source={{ uri: pdfUrl, cache: true }}
-                onLoadComplete={(numberOfPages, filePath) => {
-                    console.log(`Número de páginas: ${numberOfPages}`);
-                }}
-                onPageChanged={(page, numberOfPages) => {
-                    console.log(`Página actual: ${page}`);
-                }}
-                onError={(error) => {
-                    console.log(`Error al cargar el PDF: ${error}`);
-                }}
-                style={styles.pdfView}
-            /> */}
 
-            {/* <Pdf
-                source={{ uri: 'http://www.pdf995.com/samples/pdf.pdf', cache: true }}
+            {/* <Text style={styles.title}>Bug Ninza</Text>
+            <Pdf
+                trustAllCerts={false}
+                source={PdfResource}
+                style={styles.pdf}
                 onLoadComplete={(numberOfPages, filePath) => {
                     console.log(`number of pages: ${numberOfPages}`);
                 }}
-                onPageChanged={(page, numberOfPages) => {
-                    console.log(`current page: ${page}`);
-                }}
-                onError={(error) => {
-                    console.log(error);
-                }}
             /> */}
+
+            {/* <WebView
+                source={{ uri: 'https://playground.undo.mx/Out/Polizas/4/6040035690.pdf' }}
+                scalesPageToFit={true}
+                onError={(error) => console.log('Error en WebView:', error)}
+                originWhitelist={['*']}
+                allowsFullscreenVideo={true}
+                javaScriptEnabled={true}
+                injectedJavaScript={'(function() { })()'}
+            /> */}
+
+            {/* <WebView
+                bounces={false}
+                scrollEnabled={false}
+                source={{ uri: 'https://drive.google.com/viewerng/viewer?embedded=true&url=http://www.africau.edu/images/default/sample.pdf' }} />
+         */}
 
         </View>
     );
@@ -65,12 +146,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    pdfView: {
-        flex: 1,
-        width: 300,
-        height: 400,
-    },
+    }
 });
 
 export default PDFViewerScreen;
