84d1870ad7ad52702fe812002103f5e3b708c5e0
Yadira Perez Castillo <yadira.perez@rodac.com.mx>
2024-02-23 13:25:56 -0600

    cambiosd fionales finales app.js jazmin

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from './component/Login/HomeScreen';
 import LoginScreen from './component/Login/LoginScreen';
 import GruposScreen from './component/Login/GrupoScreen';
 import ClientesScreen from './component/Login/ClientesScreen';
@@ -15,11 +13,8 @@ import ModulosScreen from './component/Login/ModulosScreen';
 import CotizacionAutosScreen from './component/Autos/CotizacionAutosScreen';
 import ResultadoCotizacionScreen from './component/Autos/ResultadoCotizacionScreen';
 import EmisionScreen from './component/Autos/EmisionScreen';
-import SideMenu from './component/Componentes/SideMenu';
-import MenuComponentNew from './component/Componentes/MenuComponentNew';
 import PDFViewerScreen from './component/Componentes/PDFViewerScreen';
 import ViewerBase64Screen from './component/Componentes/ViewerBase64Screen';
-import ConsultaPolizasScreen from './component/Polizas/ConsultaPolizasScreen';
 import InicioAPScreen from './component/Agropecuario/InicioAPScreen';
 import MiPerfilScreen from './component/Polizas/MiPerfilScreen';
 import NewPolizas from './component/Polizas/NewPolizas';
@@ -30,16 +25,8 @@ import { Ionicons } from '@expo/vector-icons';
 import MenuButtonAndModal from './component/Componentes/YourModal';
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
-const DrawerNavigator = () => (
-  <Drawer.Navigator drawerContent={(props) => <SideMenu {...props} />}>
-    <Drawer.Screen name="CotizacionAutos" component={CotizacionAutosScreen} initialParams={{ DataParameter: '' }} />
-  </Drawer.Navigator>
-);
 
 export default function App() {
-
 
   Notifications.setNotificationHandler({
     handleNotification: async () => ({
@@ -90,20 +77,13 @@ export default function App() {
   const notificationListener = useRef();
   const responseListener = useRef();
   const [loading, setLoading] = useState(true);
-  const [isModalMenuVisible, setModalMenuVisible] = useState(false);
 
   useEffect(() => {
-    // registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-    // console.log("TOKENNNNNN",expoPushToken)
+
     const fetchExpoPushToken = async () => {
       try {
-
         const token = await registerForPushNotificationsAsync();
         setExpoPushToken(token);
-        //console.log("TOKENNNNNN",token)
-        //registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-        //console.log("TOKENNNNNN",expoPushToken)
-
       } catch (error) {
         console.error('Error obtaining Expo Push token:', error);
       } finally {
@@ -116,7 +96,6 @@ export default function App() {
     });
 
     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log("resss", response);
     });
 
     return () => {
@@ -127,7 +106,6 @@ export default function App() {
   }, []);
 
   if (loading) {
-    // Puedes mostrar un componente de carga mientras esperas que el token esté disponible
     return (
       <View style={styles.loadingContainer}>
         <ActivityIndicator size="large" color="#0000ff" />
@@ -152,39 +130,9 @@ export default function App() {
     },
   };
 
-  // const exitButton = (navigation) => (
-  //   <TouchableOpacity
-  //     onPress={() => {
-  //       navigation.navigate('Login');
-  //     }}
-  //     style={{ marginRight: 10 }}
-  //   >
-  //     <Ionicons name="exit" size={24} color="white" />
-  //   </TouchableOpacity>
-  // );
-
   const headerBackImageFuntion = () => (
     <View style={{ marginLeft: 15 }}><Ionicons name="arrow-back" size={24} color="white" /></View>
   );
-
-  // const homeAndExitButtons = (navigation, route) => (
-  //   <View style={{ flexDirection: 'row', marginRight: 20 }}>
-  //     <TouchableOpacity
-  //       onPress={() => {
-  //         let params = { DataParameter: route.params?.DataParameter };
-  //         if (route.name === 'ResultadoCotizacion') {
-  //           params = { DataParameter: route.params?.dataArray?.DataParameter };
-  //         } else if (route.name === 'Emision') {
-  //           params = { DataParameter: route.params?.dataArrayEmi?.DataParameter };
-  //         }
-  //         navigation.navigate('Modulos', params);
-  //       }}
-  //       style={{ marginBottom: 10, marginRight: 10 }}>
-  //       <Ionicons name="home" size={24} color="white" />
-  //     </TouchableOpacity>
-  //     {exitButton(navigation)}
-  //   </View>
-  // );
 
   return (
 
@@ -208,7 +156,6 @@ export default function App() {
           options={({ route, navigation }) => ({
             ...defaultHeaderOptions,
             headerLeft: null,
-            //headerRight: () => exitButton(navigation),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -253,7 +200,6 @@ export default function App() {
             ...defaultHeaderOptions,
             title: 'Modulos',
             headerBackImage: () => headerBackImageFuntion(),
-            //headerRight: () => ButtonMenu(navigation),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -265,7 +211,6 @@ export default function App() {
             ...defaultHeaderOptions,
             title: 'Cotizar Autos',
             headerBackImage: () => headerBackImageFuntion(),
-            //headerRight: () => exitButton(navigation),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -277,7 +222,6 @@ export default function App() {
             ...defaultHeaderOptions,
             title: 'Cotización',
             headerBackImage: () => headerBackImageFuntion(),
-            // headerRight: () => homeAndExitButtons(navigation, route),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -288,7 +232,6 @@ export default function App() {
           options={({ route, navigation }) => ({
             ...defaultHeaderOptions,
             title: 'Emisión',
-            //headerRight: () => homeAndExitButtons(navigation, route),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -299,7 +242,6 @@ export default function App() {
           options={({ route, navigation }) => ({
             ...defaultHeaderOptions,
             title: 'Documento',
-            //headerRight: () => homeAndExitButtons(navigation, route),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -316,7 +258,6 @@ export default function App() {
             ...defaultHeaderOptions,
             title: 'Mis Pólizas',
             headerBackImage: () => headerBackImageFuntion(),
-            // headerRight: () => exitButton(navigation),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
@@ -333,13 +274,10 @@ export default function App() {
             ...defaultHeaderOptions,
             title: 'Mi Perfil',
             headerBackImage: () => headerBackImageFuntion(),
-            //headerRight: () => homeAndExitButtons(navigation,route),
             headerRight: () => <MenuButtonAndModal navigation={navigation} route={route} />,
           })}
         />
-
       </Stack.Navigator>
-
     </NavigationContainer>
 
   );
@@ -353,24 +291,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  modalContainer: {
-    // justifyContent: 'flex-end',
-    // alignItems: 'flex-end',
-    // justifyContent: 'flex-end',
-    // margin: 0,
-    // backgroundColor: 'red',
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-  },
-  modalContent: {
-    backgroundColor: '#001F3F',
-    padding: 16,
-    height: '100%',
-    width: '50%',
-    borderTopLeftRadius: 20,
-    borderBottomLeftRadius: 20,
-    position: 'absolute',
-    right: 0,
-  },
+
 });
